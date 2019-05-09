548a3c81e0d1daaa64edd1199b0b63df37e663b7
Ahmad Rasoulpour <ahmad.raman83@yahoo.com>
2019-05-09 11:27:04 -0600

    filter component

---
+++ customer-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<app-customers></app-customers>`
+})
+export class AppComponent {
+  constructor(){}
+
+  ngOnInit(){
+  }
+}
+
+// first component that will load
